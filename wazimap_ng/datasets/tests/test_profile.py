d420d8d7fb4cf04f24e378cd9cf37c35a857dea8
BarisSari <sariba@itu.edu.tr>
2020-01-12 17:05:30 +0300

    Add cache clear, update all tests

--- wazimap_ng/datasets/tests/test_profile.py
+++ wazimap_ng/datasets/tests/test_profile.py
@@ -10,15 +10,13 @@ from wazimap_ng.datasets.models import (
 from rest_framework import status
 from rest_framework.reverse import reverse
 from django.test import TestCase
-from nose.tools import eq_
-from faker import Faker
-
-fake = Faker(seed=123456789)
+from django.core.cache import cache
 
 
 class ProfileTestCase(TestCase):
-    def setUp(self) -> None:
-        self.first_dataset = Dataset.objects.create(name="first")
+    def setUp(self):
+        cache.clear()
+        self.first_dataset = Dataset.objects.create(name="test")
         self.first_profile = Profile.objects.create(name="first_profile")
         indicator = Indicator.objects.create(
             name="first_indicator",
@@ -41,28 +39,23 @@ class ProfileTestCase(TestCase):
         )
         self.second_profile = Profile.objects.create(name="second_profile")
 
-    def tearDown(self) -> None:
-        Dataset.objects.all().delete()
-        Profile.objects.all().delete()
-        Indicator.objects.all().delete()
-
     def test_correct_profile_list_returned(self):
         url = reverse("profile-list")
         response = self.client.get(url)
-        eq_(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         results = response.data["results"]
-        eq_(len(results), 2)
-        eq_(results[0]["name"], "first_profile")
-        eq_(results[1]["name"], "second_profile")
+        self.assertEqual(len(results), 2)
+        self.assertEqual(results[0]["name"], "first_profile")
+        self.assertEqual(results[1]["name"], "second_profile")
 
     def test_correct_profile_returned(self):
         pk = self.first_profile.pk
         url = reverse("profile-detail", kwargs={"pk": pk})
         response = self.client.get(url, format="json")
-        eq_(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         results = response.data
-        eq_(results["name"], "first_profile")
-        eq_(results["indicators"][0]["subcategory"], "sub_category_1")
-        eq_(results["indicators"][0]["category"], "category_1")
+        self.assertEqual(results["name"], "first_profile")
+        self.assertEqual(results["indicators"][0]["subcategory"], "sub_category_1")
+        self.assertEqual(results["indicators"][0]["category"], "category_1")
